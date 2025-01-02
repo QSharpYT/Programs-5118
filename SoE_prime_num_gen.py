7e7c04ee84b2c4103f3cdc62df7319394eabebed
Agustya Girirajan <qsharptech@gmail.com>
2025-01-02 17:10:28 -0500

    Add files via upload

---
+++ SoE_prime_num_gen.py
@@ -0,0 +1,19 @@
+def Sieve_of_Eratosthenes(n):
+    #Makes lists for the prime numbers, the composite numbers, and the numbers not sorted yet
+    notSorted = []
+    notPrimes = []  #Also known as composite numbers
+    primes = []
+    p = 2  #Making the smallest prime to find multiples later in the code
+    for i in range(2,n+1):
+        notSorted.append(i)
+    for p in range(2,n+1):
+        for x in range(2,n+1):
+            #Here is where we find all the multiples from the variable p
+            notPrimes.append(x * p)
+            p = p + 1 
+    for a in range(len(notSorted)):
+        if not notSorted[a] in notPrimes:
+            primes.append(notSorted[a])
+    print(primes)
+
+Sieve_of_Eratosthenes(40)
